75a63df579cb9b5413b5c0ee00a100f509803872
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-03-10 15:39:24 -0500

    Added functionality for adding users to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require('express');
+require('./src/models/User');
 require('dotenv').config({ path: './.env' });
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./src/routes/authRoutes');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoURI = `mongodb+srv://TopicalTom:${process.env.MONGO_DB_KEY}@cluster0.7zy9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance')
+});
+mongoose.connection.error('error', (err) => {
+    console.error('Error connecting to mongo', err)
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
