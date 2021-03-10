31ff4078cd1c647b006e8988bd643c55979eb192
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-03-10 17:14:08 -0500

    Added track route logic

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 require('./src/models/User');
+require('./src/models/Track');
 require('dotenv').config({ path: './.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./src/routes/authRoutes');
+const trackRoutes = require('./src/routes/trackRoutes');
 const requireAuth = require('./src/middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoURI = `mongodb+srv://TopicalTom:${process.env.MONGO_DB_KEY}@cluster0.7zy9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose.connect(mongoURI, {
